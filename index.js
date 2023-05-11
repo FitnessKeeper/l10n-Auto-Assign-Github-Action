41022ca0dbb2d1364dc7a8274586a37a9924b2cd
dcrelling <david.crelling@runkeeper.com>
2023-05-11 14:00:08 -0400

    trying to get the login of the last commit

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ try {
   console.log(`The event payload: ${payload}`);
   const token = core.getInput(GITHUB_TOKEN_KEY);
   const octokit = github.getOctokit(token)
+  //get the login of the user who made the last commit with this action
+  const { data: commit } = await octokit.repos.getCommit({  owner: github.context.repo.owner, repo: github.context.repo.repo, ref: github.context.sha }); 
+  const commitAuthor = commit.author.login;
+  console.log(`commit author: ${commitAuthor}`);
+
 } catch (error) {
   core.setFailed(error.message);
 }
