aae493acd50a6d9a7e9287c2989cc31e12aff1e6
dcrelling <david.crelling@runkeeper.com>
2023-05-11 14:56:06 -0400

    removing comments

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const GITHUB_TOKEN_KEY = 'github-token'
 
-//make code below async
-
  async function run() {
   try {
     const nameToGreet = core.getInput('who-to-greet');
@@ -27,25 +25,3 @@ const GITHUB_TOKEN_KEY = 'github-token'
 }
 
 run();
-
-
-// try {
-//   // `who-to-greet` input defined in action metadata file
-//   const nameToGreet = core.getInput('who-to-greet');
-//   console.log(`Hello ${nameToGreet}!`);
-//   const time = (new Date()).toTimeString();
-//   core.setOutput("time", time);
-//   // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   console.log(`The event payload: ${payload}`);
-//   const token = core.getInput(GITHUB_TOKEN_KEY);
-//   const octokit = github.getOctokit(token)
-//   octokit.rest.issues.addAssignees({
-//     owner: github.context.payload.repository.owner.login,
-//     repo: github.context.payload.repository.name,
-//     issue_number: github.context.payload.pull_request.number,
-//     assignees: ['dcrelling']
-//   });
-// } catch (error) {
-//   core.setFailed(error.message);
-// }
