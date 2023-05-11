a0d1e67d44521688f4d214b0960663bc2e3e79cb
dcrelling <david.crelling@runkeeper.com>
2023-05-11 14:53:20 -0400

    making async

--- index.js
+++ index.js
@@ -2,23 +2,50 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const GITHUB_TOKEN_KEY = 'github-token'
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-  const token = core.getInput(GITHUB_TOKEN_KEY);
-  const octokit = github.getOctokit(token)
-  octokit.rest.issues.addAssignees({
-    owner: github.context.payload.repository.owner.login,
-    repo: github.context.payload.repository.name,
-    issue_number: github.context.payload.pull_request.number,
-    assignees: ['dcrelling']
-  });
-} catch (error) {
-  core.setFailed(error.message);
+//make code below async
+
+ async function run() {
+  try {
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+    const token = core.getInput(GITHUB_TOKEN_KEY);
+    const octokit = github.getOctokit(token)
+    octokit.rest.issues.addAssignees({
+      owner: github.context.payload.repository.owner.login,
+      repo: github.context.payload.repository.name,
+      issue_number: github.context.payload.pull_request.number,
+      assignees: ['dcrelling']
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
+
+
+// try {
+//   // `who-to-greet` input defined in action metadata file
+//   const nameToGreet = core.getInput('who-to-greet');
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = (new Date()).toTimeString();
+//   core.setOutput("time", time);
+//   // Get the JSON webhook payload for the event that triggered the workflow
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event payload: ${payload}`);
+//   const token = core.getInput(GITHUB_TOKEN_KEY);
+//   const octokit = github.getOctokit(token)
+//   octokit.rest.issues.addAssignees({
+//     owner: github.context.payload.repository.owner.login,
+//     repo: github.context.payload.repository.name,
+//     issue_number: github.context.payload.pull_request.number,
+//     assignees: ['dcrelling']
+//   });
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
