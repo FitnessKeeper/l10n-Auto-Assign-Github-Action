75f8ed2501a1574fba4fcca52d287a5c7a23068c
dcrelling <david.crelling@runkeeper.com>
2023-05-12 12:53:41 -0400

    trying

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ const GITHUB_TOKEN_KEY = 'github-token'
 
     //loop through commits and log the author name
     commits.data.forEach((commit) => {
-      console.log(commit.author.name)
-      console.log(commit.committer.name)
+      console.log("here")
+      console.log(commit)
     })
 
     console.log(commits)
